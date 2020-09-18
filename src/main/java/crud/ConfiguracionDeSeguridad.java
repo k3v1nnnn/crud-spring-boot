2feeeff17c648bceeefa0cda2bafd236bcf4a4a0
kevin <kevinnnnmarcelo@gmail.com>
2020-09-18 09:51:23 -0300

    feat: Configuracion de permisos para las paginas

--- src/main/java/crud/ConfiguracionDeSeguridad.java
+++ src/main/java/crud/ConfiguracionDeSeguridad.java
@@ -1,10 +1,22 @@
 package crud;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionDeSeguridad extends WebSecurityConfigurerAdapter{
-
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/actualizarEmpleado/**").hasRole("ADMIN")
+            .antMatchers("/agregarEmpleado/**").hasRole("ADMIN")
+            .antMatchers("/eliminarEmpleado/**").hasRole("ADMIN")
+            .anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/iniciarSesion").permitAll();
+    }
 }
