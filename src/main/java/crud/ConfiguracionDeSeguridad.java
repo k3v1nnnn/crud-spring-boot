8f89a1f025960efdcb0efa96db46e3bf1320acea
kevin <kevinnnnmarcelo@gmail.com>
2020-09-19 00:08:43 -0300

    feat: Agregamos usuarios y contraseña

--- src/main/java/crud/ConfiguracionDeSeguridad.java
+++ src/main/java/crud/ConfiguracionDeSeguridad.java
@@ -1,7 +1,9 @@
 package crud;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
@@ -17,6 +19,21 @@ public class ConfiguracionDeSeguridad extends WebSecurityConfigurerAdapter{
             .anyRequest().authenticated()
 			.and()
 			.formLogin()
-			.loginPage("/iniciarSesion").permitAll();
+			.loginPage("/iniciarSesion").permitAll()
+			.defaultSuccessUrl("/").permitAll()
+            .and()
+            .logout()
+            .logoutUrl("/cerrarSesion") 
+            .logoutSuccessUrl("/").permitAll();
+    }
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("admin").roles("ADMIN");
+    }
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+        .antMatchers("/resources/**", "/static/**","/webjars/**");
     }
 }
---
+++ src/main/java/crud/controller/IniciarSesionController.java
@@ -0,0 +1,14 @@
+package crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/iniciarSesion")
+public class IniciarSesionController {
+	@GetMapping("")
+	public String iniciarSesion() {
+		return "iniciarSesion";
+	}
+}
